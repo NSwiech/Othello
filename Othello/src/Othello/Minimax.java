942115be5758ab2f2f6f1e94f13e227cab5bd3d1
NicklasP <yberguru@gmail.com>
2016-09-25 16:10:35 +0200

    Minimax started

---
+++ Othello/src/Othello/Minimax.java
@@ -0,0 +1,18 @@
+package Othello;
+
+public class Minimax {
+	private final int MAX_DEAPTH = 16; //search cut-off
+	
+	//TODO
+	//The following shall be printed after each draw:
+	//  The current status of the board --CHECK
+	//  The depth of the search
+	//  How many nodes were examined
+	//
+	//A computer draw shall not take longer than 5 seconds
+
+	private Slot[][] board = new Slot[4][4];
+	private Position action = new Position();
+	
+	
+}
