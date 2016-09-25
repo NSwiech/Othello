07b44058a85419b3e6877985a6eb42cd0bf3340a
Nicolas Swiech <nicolas.swiech@gmail.com>
2016-09-25 03:09:40 +0200

    edited getters ; added attributes

--- Othello/src/Othello/Computer.java
+++ Othello/src/Othello/Computer.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class Computer implements Controllable {
 
 	private String color;
-
+	private Scanner rowChoice = new Scanner(System.in);
+	private Scanner colChoice = new Scanner(System.in);
+	private int row, col;
+	
 	public Computer(String color) {
 		this.color = color;
 	}
@@ -13,14 +16,21 @@ public class Computer implements Controllable {
 	public void play() {
 		boolean played = false;
 		while (!played) {
+			
+			System.out.println("Choose a row between 1 and 4");
+			row = this.rowChoice.nextInt() - 1;
+			System.out.println("Choose a column between 1 and 4");
+			col = this.colChoice.nextInt() - 1;
 
-			if (Game.grid[1][1].state == "available") {
-				
+			if (Game.grid[row][col].state == "available") {
+				System.out.println("Legal move. You played.");
+				Game.grid[row][col].state = "Black";
 				played = true;
 			} else {
-			
+				System.out.println("Not legal move.");
 			}
 		}
+		return;
 	}
 
 	/*
@@ -29,15 +39,15 @@ public class Computer implements Controllable {
 	 */
 
 	public int getLastRowPlayed() {
-		return (1);
+		return this.row;
+		
 	}
 
 	public int getLastColPlayed() {
-		return (1);
+		return this.col;
 	}
 
 	public String getColor() {
 		return this.color;
 	}
-
 }
