a5721d5d4a0dfd433a1a983b564705113060d7fb
Nicolas Swiech <nicolas.swiech@gmail.com>
2016-09-26 17:14:36 +0200

    Added input validation (needs more testing)

--- Othello/src/Othello/Human.java
+++ Othello/src/Othello/Human.java
@@ -1,36 +1,50 @@
 package Othello;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class Human implements Controllable {
 
 	private String color;
-	private Scanner rowChoice = new Scanner(System.in);
-	private Scanner colChoice = new Scanner(System.in);
 	private int row, col;
-	//TODO improve input validation suggest input char and using parse to integer 
-	//and regular expressions
-	//http://stackoverflow.com/questions/2496239/how-do-i-keep-a-scanner-from-throwing-exceptions-when-the-wrong-type-is-entered
 
-	
+	// TODO improve input validation suggest input char and using parse to
+	// integer
+	// and regular expressions
+	// http://stackoverflow.com/questions/2496239/how-do-i-keep-a-scanner-from-throwing-exceptions-when-the-wrong-type-is-entered
+
 	public Human(String color) {
 		this.color = color;
 	}
 
 	public void play() {
 		boolean played = false;
-		
-	   
+
 		while (!played) {
 			// Human enters 1-4 computer subtracts 1 to apply to arrays
-			do{
+
+			do {
 				System.out.println("Choose a row between 1 and 4");
-				 row = this.rowChoice.nextInt() - 1;
-			}while ((row>=1)&&(row<=4)); //This is NOT a solution
-			do{
+				Scanner rowChoice = new Scanner(System.in);
+				try {
+					row = rowChoice.nextInt() - 1;
+
+				} catch (InputMismatchException e1) {
+					System.out.println("Wrong input!\n");
+				}
+			} while ((row < 0) || (row > 5));
+
+			do {
 				System.out.println("Choose a column between 1 and 4");
-				 col = this.colChoice.nextInt() - 1;
-			}while ((col>=1)&&(col<=4)); //This is NOT a solution
-			
+				Scanner colChoice = new Scanner(System.in);
+				try {
+					col = colChoice.nextInt() - 1;
+
+				} catch (InputMismatchException e2) {
+					System.out.println("Wrong input!\n");
+				}
+			} while ((col < 0) || (col > 5));
+
 			if (Game.grid[row][col].state == "available") {
 				System.out.println("Legal move. You played.");
 				Game.grid[row][col].state = "White";
@@ -49,7 +63,7 @@ public class Human implements Controllable {
 
 	public int getLastRowPlayed() {
 		return this.row;
-		
+
 	}
 
 	public int getLastColPlayed() {
