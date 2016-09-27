34448817170c8042c42b6e4d84f055b3c8584611
NicklasP <yberguru@gmail.com>
2016-09-27 22:03:02 +0200

    Added class Node prototype to build tree with UNfinished

--- Othello/src/Othello/Minimax.java
+++ Othello/src/Othello/Minimax.java
@@ -1,5 +1,7 @@
 package Othello;
 
+
+
 public class Minimax {
 	private final int MAX_DEPTH = 16; //search cut-off
 	private int searchdepth; //  The depth of the search
@@ -17,4 +19,25 @@ public class Minimax {
 
 	
 	
+}
+class Node{
+	private Node parentNode;//nodeID instead perhaps? 
+	private Node leftChildNode; //nodeID instead perhaps?
+	private Node rightChildNode; //nodeID instead perhaps?
+	private String nodeID; //hash hmmmm howto do?
+	private int value = Integer.MIN_VALUE;
+	private int alfa = Integer.MIN_VALUE;
+	private int beta = Integer.MAX_VALUE;
+	
+	//Constructor
+	public Node(int v,int a,int b, Node parNode){
+		this.value = v;
+		this.alfa = a;
+		this.beta = b;
+		this.parentNode = parNode;
+	}
+	
+	public String toString(){
+		return "ID:" + nodeID + "Value:" + value + " alfa:" + alfa + ", beta:" + beta;
+	}
 }
