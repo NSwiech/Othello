52909250fe9af5e5efbd7bde62fc8bc060522961
Nicolas Swiech <nicolas.swiech@gmail.com>
2016-09-28 13:27:57 +0200

    edited in a more object oriented syntax

--- Othello/src/Othello/GameState.java
+++ Othello/src/Othello/GameState.java
@@ -1,61 +1,55 @@
 package Othello;
 
 public class GameState {
-	
+
 	// Some function is called in this class that will recieve the updated grid.
 	// From this grid create a set of available moves for the heavy algorithm.
 	//
-	private static Slot[][] gState;
-	private static Action[] availMoves;
-	
-	public static Slot[][] getgState() {
-		return gState;
+	private Slot[][] gState;
+	private Action[] availMoves;
+
+	public Slot[][] getgState() {
+		return this.gState;
 	}
 
-	public static void setgState(Slot[][] gState) {
-		GameState.gState = gState;
-	}
-	
-	public static Action[] getAvailMoves() {
-		return availMoves;
+	public void setgState(Slot[][] gState) {
+		this.gState = gState;
 	}
 
-	public static void setAvailMoves(Action[] availMoves) {
-		GameState.availMoves = availMoves;
+	public Action[] getAvailMoves() {
+		return this.availMoves;
 	}
 
-	//Constructor
-	public GameState(Slot[][] status){
+	public void setAvailMoves(Action[] availMoves) {
+		this.availMoves = availMoves;
+	}
+
+	// Constructor
+	public GameState(Slot[][] status) {
 		gState = status;
 	};
-	
-	private static class Dir{
+
+	private static class Dir {
 		private int row;
 		private int col;
-		
-		private Dir(int y, int x){
+
+		private Dir(int y, int x) {
 			this.row = y;
 			this.col = x;
 		}
 	};
+
 	// Array of all directions
-	//{{-1,0}{-1,1}{0,1}{1,1}{1,0}{1,-1}{0,-1}{-1,-1}}
-	//   N,     NE,   E,  SE,   S,   SW,    W,     NW
-	Dir N  = new Dir(-1,0);
-	Dir NE = new Dir(-1,1);
-	Dir E  = new Dir(0,1);
-	Dir SE = new Dir(1,1);
-	Dir S  = new Dir(1,0);
-	Dir SW = new Dir(1,-1);
-	Dir W  = new Dir(0,-1);
-	Dir NW = new Dir(-1,-1);
-	Dir[] dirs = {N,NE,E,SE,S,SW,W,NW};
-	
-
-	
-
+	// {{-1,0}{-1,1}{0,1}{1,1}{1,0}{1,-1}{0,-1}{-1,-1}}
+	// N, NE, E, SE, S, SW, W, NW
+	Dir N = new Dir(-1, 0);
+	Dir NE = new Dir(-1, 1);
+	Dir E = new Dir(0, 1);
+	Dir SE = new Dir(1, 1);
+	Dir S = new Dir(1, 0);
+	Dir SW = new Dir(1, -1);
+	Dir W = new Dir(0, -1);
+	Dir NW = new Dir(-1, -1);
+	Dir[] dirs = { N, NE, E, SE, S, SW, W, NW };
 
 }
-
-                                                                                  
-
