a052dfabd2f5edd066e27a693e739cce0b5013f0
Nicolas Swiech <nicolas.swiech@gmail.com>
2016-09-29 20:21:15 +0200

    Update Computer.java

--- Othello/src/Othello/Computer.java
+++ Othello/src/Othello/Computer.java
@@ -1,5 +1,6 @@
 package Othello;
 //*****************************
+
 //Nicklas Persson
 //Nicolas Swiech
 //2016-09-28
@@ -12,57 +13,16 @@ public class Computer implements Controllable {
 
 	private String color;
 	private int row, col;
-	
+
 	public Computer(String color) {
 		this.color = color;
 	}
 
 	public void play() {
-		boolean played = false;
 		
-		while (!played) {
-			
-			boolean validRowInput = false;
-			while (!validRowInput) {
-				System.out.println("Choose a row between 1 and 4");
-				Scanner rowChoice = new Scanner(System.in);
-				try {
-					row = Integer.parseInt(rowChoice.next()) - 1;
-				}
-				
-				catch (NumberFormatException e) {
-					System.out.println("Wrong input!\n");
-				}
-				if ((row >= 0) && (row < 4)) {
-					validRowInput = true;
-				}
-			}
-
-			boolean validColInput = false;
-			while (!validColInput) {
-				System.out.println("Choose a column between 1 and 4");
-				Scanner colChoice = new Scanner(System.in);
-				try {
-					col = Integer.parseInt(colChoice.next()) - 1;
-
-				} catch (NumberFormatException e) {
-					System.out.println("Wrong input!\n");
-				}
-				if ((col >= 0) && (col < 4)) {
-					validColInput = true;
-				}
-			}
-
-			if (Game.getStateSlot(row, col) == "available") {
-				System.out.println("Legal move. You played.");
-				Game.setStateSlot(row,col,"Black");
-				played = true;
-			} else {
-				System.out.println("Not legal move.");
-			}
-		}
-		return;
+		
 	}
+
 
 	public int getLastRowPlayed() {
 		return this.row;
